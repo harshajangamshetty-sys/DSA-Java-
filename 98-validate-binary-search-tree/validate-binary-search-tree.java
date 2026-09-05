40bb1681b5adb407fb409073a8c2b724fc316f88
HARSHA JANGAMSHETTY <harshajangamshetty@gmail.com>
2026-09-05 19:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (50.75%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+         return  dfs(root , Long.MIN_VALUE ,Long.MAX_VALUE);
+
+    }
+
+    public boolean dfs (TreeNode node , long lowerBound , long upperBound) {
+        if(node == null) {
+            return true;
+        }
+
+
+        if(node.val <= lowerBound || node.val >= upperBound) {
+            return false;
+        }
+
+       boolean left = dfs(node.left , lowerBound , node.val);
+       boolean right = dfs(node.right , node.val , upperBound);
+
+        return left && right;
+    }
+}
